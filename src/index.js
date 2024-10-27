2358ff92211eb2417f09da765e73e4d211a2b0d5
tarasovcad <tarasovcad@gmail.com>
2024-10-27 15:28:24 -0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openBrackets = bracketsConfig.map(pair => pair[0]);
+  const closeBrackets = bracketsConfig.map(pair => pair[1]);
+  const bracketsMap = Object.fromEntries(bracketsConfig);
+
+  for (const char of str) {
+      if (openBrackets.includes(char)) {
+         
+          if (char === bracketsMap[char] && stack[stack.length - 1] === char) {
+              stack.pop(); 
+          } else {
+              stack.push(char); 
+          }
+      } else if (closeBrackets.includes(char)) {
+          const last = stack.pop();
+          if (bracketsMap[last] !== char) return false; 
+      }
+  }
+
+  return stack.length === 0; 
 }
